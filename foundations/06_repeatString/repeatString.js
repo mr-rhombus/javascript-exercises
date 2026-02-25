1fdfdc25cb4fe1a63beefa0d285cc76420949823
mr-rhombus <diamax98@gmail.com>
2026-02-25 18:53:00 -0500

    Finish JS foundations ex. 6
    Practice using loops and conditional statements to implement a custom
    function that repeats a string a specific number of times. This is meant
    to roughly emulate the `repeat()` string method.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR'
+    }
 
+    let counter = 0;
+    let out = '';
+    while (counter < num) {
+        out += string;
+        counter++;
+    }
+    return out
 };
 
 // Do not edit below this line
