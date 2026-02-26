e82e118754263fc2f1ab1775a288ac9d3bf43e7b
mr-rhombus <diamax98@gmail.com>
2026-02-25 19:55:05 -0500

    Finish JS foundations ex. 9
    Create a function that accepts 2 nonnegative integers and sums all
    integers between them, including the endpoints. The function should
    return `'ERROR'` for any invalid inputs.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+        return 'ERROR'
+    }
+    let start = a <= b ? a : b;
+    let end = b >= a ? b: a;
+    let i = 0;
+    let res = 0;
+    while ((start + i) <= end) {
+        res += (start + i);
+        i++;
+    }
+    return res;
 };
 
 // Do not edit below this line
