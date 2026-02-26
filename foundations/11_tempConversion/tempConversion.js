f0b41902380fd66fc28898586ffa7dbc0e5a5c1c
mr-rhombus <diamax98@gmail.com>
2026-02-25 21:04:46 -0500

    Finish JS foundations ex. 11
    Write 2 functions that convert temperatures between F and C. Create a
    third function to help round the converted temperatures to 1 decimal
    place.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  return round((5/9) * (temp - 32), 1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return round((9/5) * temp + 32, 1)
 };
+
+const round = function(num, decimals) {
+  return parseFloat(num.toFixed(decimals))
+}
 
 // Do not edit below this line
 module.exports = {
