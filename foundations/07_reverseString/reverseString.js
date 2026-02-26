518700de731bcf23c9213f920aa063c75ff988af
mr-rhombus <diamax98@gmail.com>
2026-02-25 19:01:19 -0500

    Finish JS foundations ex. 7
    Create a function that reverses a string without using a for loop.

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let out = '';
+    let i = 0;
+    while (i < string.length) {
+        out = string[i] + out;
+        i++;
+    }
+    return out
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
