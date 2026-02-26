e523b9b47c441f759667c5deae95a16fbb741548
mr-rhombus <diamax98@gmail.com>
2026-02-25 19:32:19 -0500

    Finish JS foundations ex. 8
    Create a function that removes an unspecified number of elements from
    an array. This exercise enforces good programmatic thinking,
    consideration of edge cases, and making use of the `arguments` JS obj.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    let i = 0;
+    while (i < arguments.length - 1) {
+        let found = true;
+        let toRemove = arguments[i+1]  // Skip over arr
+        while (found) {
+            // Handle duplicate array elements that need removing
+            let firstMatchIx = arr.indexOf(toRemove)
+            // Element not found in array
+            if (firstMatchIx == -1) {
+                found = false;
+            } else {
+                arr.splice(firstMatchIx, 1)
+            }
+        }
+        i++;
+    }
+    return arr
 };
 
 // Do not edit below this line
