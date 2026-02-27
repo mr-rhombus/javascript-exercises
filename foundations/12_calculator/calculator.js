53c97cf9fc0814cacc18c4a13ea1028d8c1adac5
mr-rhombus <diamax98@gmail.com>
2026-02-27 15:56:07 -0500

    Finish JS foundations ex. 12
    Create several helper functions that emulate a calculator including
    addition, subtraction, multiplication, exponentiation, and factorials

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let res = 0;
+  let i = 0;
+  while (i < arr.length) {
+    res += arr[i]
+    i++
+  }
+  return res
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let res = 1;
+  let i = 0;
+  while (i < arr.length) {
+    res *= arr[i]
+    i++
+  }
+  return res
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return base ** exp
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let res = 1;
+  let i = 0;
+  let increment;
+  if (num > 0) {
+    increment = -1;
+  } else {
+    increment = 1;
+  }
+  while (Math.abs(i) < Math.abs(num)) {
+    res *= (num + increment * i);
+    i++;
+  }
+  return res
 };
 
 // Do not edit below this line
