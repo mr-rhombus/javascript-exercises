209d0776c27c0e4529a5c5f4871eba43b9969c40
mr-rhombus <diamax98@gmail.com>
2026-02-28 11:17:28 -0500

    Finish JS foundations ex. 13
    Create a function that detects if a string is a palindrome. The function
    should ignore punctuation and whitespace. It should allow for any
    valid alphanumeric character. It should be case insensitive (i.e.
    'Racecar' is a valid palindrome).
    
    I created several helper functions to simplify the `palindromes`
    function logic.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,52 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let cleanString = onlyAlphaNumeric(string);
+  let cleanStringReversed = reversed(cleanString);
+  let i = 0;
+  while (i < cleanString.length) {
+    // Letters don't match
+    if ( !lettersAreEqual(cleanString[i], cleanStringReversed[i]) ) {
+      return false;
+    };
+    i++;
+  }
+  return true;
+}
 
-};
+const isLetter = function (el) {
+  let letterRe = /[a-zA-Z]/;
+  return letterRe.exec(el) == null ? false : true;
+}
+
+const isNumber = function (el) {
+  let numberRe = /[0-9]/
+  return numberRe.exec(el) == null ? false : true;
+}
+
+const lettersAreEqual = function (a, b) {
+  return a.toLowerCase() == b.toLowerCase()
+}
+
+const onlyAlphaNumeric = function(a) {
+  let ans = '';
+  let i = 0;
+  while (i < a.length) {
+    if ( isLetter(a[i]) || isNumber(a[i] )) {
+      ans += a[i];
+    }
+    i++;
+  }
+  return ans;
+}
+
+const reversed = function (string) {
+  let i = 0;
+  let ans = '';
+  while (i < string.length) {
+    ans += string[(string.length - 1) - i]
+    i++;
+  }
+  return ans
+}
 
 // Do not edit below this line
 module.exports = palindromes;
